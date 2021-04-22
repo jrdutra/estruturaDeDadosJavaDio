21d180ee6ee1d1adc045e84c6c9119e30f9a1705
João Dutra <jrdutra.com.br@gmail.com>
2021-04-21 22:20:53 -0300

    Upload apresentações pptx

---
+++ projetos/Aula1/Video4-InstalacaoEConfiguracaoDoAmbiente/primeiroProjeto/src/main/java/com/estruturadedados/Main.java
@@ -0,0 +1,7 @@
+package com.estruturadedados;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Bem vindo ao curso de estrutura de Dados em Java!");
+    }
+}
