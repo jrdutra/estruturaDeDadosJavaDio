509395eddbe84daa296a92e7c32e75aaa400701c
João Dutra <jrdutra.com.br@gmail.com>
2021-04-25 22:33:56 -0300

    Inclusão dos projetos das etapas 2 e 3 da Aula4

---
+++ projetos/Aula4/Etapa3/listaencadeada/src/main/java/one/digitalinnovation/Main.java
@@ -0,0 +1,27 @@
+package one.digitalinnovation;
+
+public class Main {
+
+    public static void main(String args[]){
+
+        ListaEncadeada<String> minhaListaEncadeada = new ListaEncadeada<>();
+
+        minhaListaEncadeada.add("teste1");
+        minhaListaEncadeada.add("teste2");
+        minhaListaEncadeada.add("teste3");
+        minhaListaEncadeada.add("teste4");
+
+        System.out.println(minhaListaEncadeada.get(0));
+        System.out.println(minhaListaEncadeada.get(1));
+        System.out.println(minhaListaEncadeada.get(2));
+        System.out.println(minhaListaEncadeada.get(3));
+
+        minhaListaEncadeada.remove(3);
+
+        System.out.println(minhaListaEncadeada);
+
+
+
+    }
+
+}
